c7adbbb2175da79dd5ca9bb6bdbb0b3a8b34fb76
CodeSonia <sonia_ah@icloud.com>
2021-08-23 19:57:19 +0000

    using jQuery event handlers

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Event Handler
 $(document).ready(function() {
   $("#stream1_btn").on("click", function() {
       $(".stream1").removeClass('highlight_stream');
@@ -5,6 +6,67 @@ $(document).ready(function() {
       $(".stream3").removeClass('highlight_stream');
       $(".stream1").addClass('highlight_stream');
  });
+ $("#stream2_btn").on("click", function() {
+      $(".stream1").removeClass('highlight_stream');
+      $(".stream2").removeClass('highlight_stream');
+      $(".stream3").removeClass('highlight_stream');
+      $(".stream2").addClass('highlight_stream');
+ });
+ $("#stream3_btn").on("click", function() {
+      $(".stream1").removeClass('highlight_stream');
+      $(".stream2").removeClass('highlight_stream');
+      $(".stream3").removeClass('highlight_stream');
+      $(".stream3").addClass('highlight_stream');
+ });
+}); 
+*/
+
+/*jQuery animation effects*/
+/*
+$(document).ready(function() {
+  $("#stream1_btn").on("click", function() {
+    //show() method, tells jQuery how fast to show the element
+      $(".stream1").show();
+      $(".stream1").show("slow");
+      $(".stream1").show("medium");
+      $(".stream1").show("fast");
+      $(".stream1").show("1000");
+ });
+*/
+
+/*
+$(document).ready(function() {
+  $("#stream1_btn").on("click", function() {
+    //show() method, tells jQuery to hide elements
+      $(".stream1").hide();
+      $(".stream1").hide("slow");
+      $(".stream1").hide("medium");
+      $(".stream1").hide("fast");
+      $(".stream1").hide("1000");
+  });
+ $("#stream2_btn").on("click", function() {
+      $(".stream1").removeClass('highlight_stream');
+      $(".stream2").removeClass('highlight_stream');
+      $(".stream3").removeClass('highlight_stream');
+      $(".stream2").addClass('highlight_stream');
+ });
+ $("#stream3_btn").on("click", function() {
+      $(".stream1").removeClass('highlight_stream');
+      $(".stream2").removeClass('highlight_stream');
+      $(".stream3").removeClass('highlight_stream');
+      $(".stream3").addClass('highlight_stream');
+ });
+}); 
+*/
+
+//jQuery toggle button works like an on and off button
+$(document).ready(function() {
+  $("#stream1_btn").on("click", function() {
+    //show() method, tells jQuery to hide elements
+      $(".stream1").toggle();
+      $(".stream1").toggle("slow");
+  });
+
  $("#stream2_btn").on("click", function() {
       $(".stream1").removeClass('highlight_stream');
       $(".stream2").removeClass('highlight_stream');
